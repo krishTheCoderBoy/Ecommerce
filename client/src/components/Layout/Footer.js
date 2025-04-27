5bb00ade8167fb5acf50b021cb006fc5d7fe9629
krishnendu <kd743452@gmail.com>
2025-04-27 23:02:25 +0530

    changes done

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footer'>
-      <h1 className='text-center'>All Right Reserved &copy; DeepBhatt</h1>
+      <h1 className='text-center'>All Right Reserved &copy; Krishnendu</h1>
       <p className='text-center mt-3'>
        <Link to="/about">About</Link>|
        <Link to="/contact">Contact</Link>|
